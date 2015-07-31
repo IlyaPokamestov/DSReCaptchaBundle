7b057a8fda3a5ded454d6797a0f4ec434222aac0
Ilya Pakamiastau <ilya_pakamiastau@epam.com>
2015-07-31 16:11:19 +0300

    Merge DS/ReCaptchaValidator into DSReCaptchaBundle

---
+++ src/Form/ReCaptchaType.php
@@ -0,0 +1,79 @@
+<?php
+/*
+* This file is part of the ReCaptcha Validator Component.
+*
+* (c) Ilya Pokamestov
+*
+* For the full copyright and license information, please view the LICENSE
+* file that was distributed with this source code.
+*/
+
+namespace DS\ReCaptchaBundle\Form;
+
+use DS\ReCaptchaBundle\Validator\ReCaptchaConstraint;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Exception\InvalidConfigurationException;
+use Symfony\Component\Form\FormView;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Intl\Locale\Locale;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+/**
+ * Form type for reCaptcha.
+ *
+ * @author Ilya Pokamestov <dario_swain@yahoo.com>
+ */
+class ReCaptchaType extends AbstractType
+{
+    const JS_API_URL = 'https://www.google.com/recaptcha/api.js';
+
+    /** @var  string */
+    protected $publicKey;
+    /** @var  string */
+    protected $locale;
+
+    public function __construct($publicKey, $locale = null)
+    {
+        if (null === $publicKey) {
+            throw new InvalidConfigurationException('The parameters "public_key" must be configured.');
+        }
+
+        $this->publicKey = $publicKey;
+
+        if (null !== $locale) {
+            $this->locale = $locale;
+        } else {
+            $this->locale = Locale::getDefault();
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function buildView(FormView $view, FormInterface $form, array $options)
+    {
+        $view->vars = array_replace($view->vars, array(
+            'public_key' => $this->publicKey,
+            'lang' => $this->locale,
+            'js_api_url' => self::JS_API_URL
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'constraints' => array(new ReCaptchaConstraint())
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return 'ds_re_captcha';
+    }
+}
---
+++ src/Validator/ReCaptchaValidator.php
@@ -0,0 +1,69 @@
+<?php
+/*
+* This file is part of the ReCaptcha Validator Component.
+*
+* (c) Ilya Pokamestov
+*
+* For the full copyright and license information, please view the LICENSE
+* file that was distributed with this source code.
+*/
+
+namespace DS\ReCaptchaBundle\Validator;
+
+use DS\Library\ReCaptcha\Http\Driver\DriverInterface;
+use DS\Library\ReCaptcha\ReCaptcha;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\ConstraintValidator;
+use Symfony\Component\Validator\Exception\InvalidArgumentException;
+
+/**
+ * ReCaptcha Validator.
+ *
+ * @author Ilya Pokamestov <dario_swain@yahoo.com>
+ */
+class ReCaptchaValidator extends ConstraintValidator
+{
+    /** @var Request */
+    protected $request;
+    /** @var  string */
+    protected $privateKey;
+    /** @var  DriverInterface */
+    protected $driver;
+
+    /**
+     * @param Request $request
+     * @param string $privateKey
+     * @param DriverInterface $driver
+     */
+    public function __construct(Request $request, $privateKey, DriverInterface $driver = null)
+    {
+        $this->request = $request;
+        $this->privateKey = $privateKey;
+        $this->driver = $driver;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function validate($value, Constraint $constraint)
+    {
+        if (!($constraint instanceof ReCaptchaConstraint)) {
+            throw new InvalidArgumentException('Use ReCaptchaConstraint for ReCaptchaValidator.');
+        }
+
+        if ($this->request->get('g-recaptcha-response', false)) {
+            $reCaptcha = new ReCaptcha(
+                $this->privateKey,
+                $this->request->getClientIp(),
+                $this->request->get('g-recaptcha-response', false)
+            );
+            $response = $reCaptcha->buildRequest($this->driver)->send();
+            if (!$response->isSuccess()) {
+                $this->context->addViolation($constraint->message);
+            }
+        } else {
+            $this->context->addViolation($constraint->message);
+        }
+    }
+}
